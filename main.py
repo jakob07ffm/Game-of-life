747de8aa28a24fe07f5b81a6eeaea3700608b2ca
Jakob__init__ <jsuetfels@gmail.com>
2024-08-21 17:36:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,84 @@
+# Rules
+# 1) A live cell with fewer than tow live neighbors dies (underpopulation)
+# 2) A live cell with tow or three neighbors remains alive (survival)
+# 3) A live cell with more than three live neighbors dies (overpopulation)
+# 4) A dead cell with exactly three live neighbors becomes alive (reproduction)
 
+import pygame
+import random
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+YELLOW = (255, 255, 0)
+
+WIDTH, HEIGHT = 800, 800
+TILE_SIZE = 20
+GRID_WIDTH = WIDTH // TILE_SIZE
+GRID_HEIGHT = HEIGHT // TILE_SIZE
+FPS = 60
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+clock = pygame.time.Clock()
+
+def gen(num):
+    return set([(random.randrange(0, GRID_HEIGHT), random.rnadrange(0, GRID_WIDTH)) for _ in range(num)])
+
+def draw_grid(positions):
+    for position in positions:
+        col, row = position
+        top_left = (col * TILE_SIZE, row * TILE_SIZE)
+        pygame.draw.rect(screen, YELLOW, (*top_left, TILE_SIZE, TILE_SIZE))
+    
+    for row in range(GRID_HEIGHT):
+        pygame.draw.line(screen, BLACK, (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE))
+
+    for col in range(GRID_WIDTH):
+        pygame.draw.line(screen, BLACK, (col * TILE_SIZE, 0), (col * TILE_SIZE, HEIGHT))
+
+def main():
+    running = True
+    playing = False
+
+    positions = set()
+    while running: 
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                col = x // TILE_SIZE
+                row = y // TILE_SIZE
+                pos = (col, row)
+
+                if pos in positions:
+                    positions.remove(pos)
+                else:
+                    positions.add(pos)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    playing = not playing
+                if event.key == pygame.K_c:
+                    position = set()
+                    playing = False
+                if event.key == pygame.K_g:
+                    position = gen(random.randrange(2, 5) * GRID_WIDTH)
+                
+
+        screen.fill(GREY)
+        draw_grid(positions)
+
+        pygame.display.update()
+        
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
+# Time in vid = 25:23
